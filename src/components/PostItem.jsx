708961288a45228eca0e2700e6b08f325f529a7d
Boyoung Lee <genuss040822@gamil.com>
2024-06-02 17:00:32 +0900

    날짜 가져오는 방식 업데이트

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { CiBookmark } from 'react-icons/ci';
+import { IoHeartOutline } from 'react-icons/io5';
+import { stringPostDate } from '../store/slices/postSlice';
 
 const StPostItem = styled.article`
   background-color: orange;
@@ -8,21 +11,46 @@ const StPostItem = styled.article`
   width: 90%;
   min-width: 285px;
   min-height: 427px;
+  border-radius: 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 5%;
+`;
+
+const StImage = styled.img`
+  width: 90%;
+  height: auto;
+  border-radius: 5%;
+`;
+
+const StIdAndLikeButtons = styled.div`
+  background-color: aqua;
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const PostItem = ({ post }) => {
+  const { image, userId, productName, postContent, popularity, postDate } = post;
+
   return (
-    <div>
-      <StPostItem>
-        <div>{post.postDate}</div>
-        <div>{post.image}</div>
-        <div>{post.userId}</div>
-        <div>{post.productName}</div>
-        <div>{post.postContent}</div>
-        <div>{post.popularity}</div>
-      </StPostItem>
-    </div>
+    <StPostItem>
+      <StImage src={image} alt={productName} />
+      <StIdAndLikeButtons>
+        {userId}
+        <span>
+          <IoHeartOutline />
+          {popularity}
+          <CiBookmark />
+        </span>
+      </StIdAndLikeButtons>
+      <div>{postContent}</div>
+      <div>{stringPostDate}</div>
+      {/* <div>{postDate}</div> */}
+    </StPostItem>
   );
 };
-
 export default PostItem;
