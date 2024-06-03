73c50e4e4ed0e0f8333eed8d04b8893807c73873
Jiyoung <jiyoung990602@gmail.com>
2024-06-04 02:09:43 +0900

    게시물 작성 페이지 머지

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ShowPostList from '../components/main/ShowPostList';
+import Carousel from '../components/main/Carousel';
+// import MainCategory from '../components/Main/MainCategory';
+
 const MainPage = () => {
   return (
     <div>
