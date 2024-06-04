d638b792a75a6046c0669bccc35c1b449f1421a9
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-06-04 09:51:54 +0900

    마이페이지

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ifzzsqrbvtphsikwxkms.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmenpzcXJidnRwaHNpa3d4a21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczMzc4MzAsImV4cCI6MjAzMjkxMzgzMH0.kWiKh0Qlwtlr4zvN-jsPsItwTEjbDtjtN1OBMmI5jxI';
-//import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
