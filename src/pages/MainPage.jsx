83adfaf764dd6fb626ef0d2cbbc0d52689da2c14
Jiyoung <jiyoung990602@gmail.com>
2024-06-04 11:16:48 +0900

    로컬 changes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ShowPostList from '../components/main/ShowPostList';
-<<<<<<< HEAD
-=======
 import Carousel from '../components/main/Carousel';
 import MainCategory from '../components/main/MainCategory';
->>>>>>> 6aae419f857eff9a0017e05ece2e67bcb51ad490
+
 const MainPage = () => {
   return (
     <div>
