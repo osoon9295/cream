2d30317748bc85e94fc35f39c21f7f652219cc9e
Leejunhyeok03 <ly0609@naver.com>
2024-06-06 10:19:49 +0900

    feat: 저장, 하트 기능 추가

--- src/components/Main/PostItem.jsx
+++ src/components/Main/PostItem.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoBookmarkOutline, IoHeartOutline } from 'react-icons/io5';
-import styled from 'styled-components';
-// import { stringPostDate } from '../../store/slices/postSlice';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { checkSignIn, getUser } from '../../api/api.auth';
 import supabase from '../../supabase';
 
 const StPostItem = styled.div`
-  /* background-color: rebeccapurple; */
   width: 90%;
   height: 90%;
   border-radius: 5%;
@@ -17,8 +15,6 @@ const StPostItem = styled.div`
   justify-content: center;
   border: none;
   margin: 0 0 20px 0;
-  @media screen and (max-width: 600px) {
-  }
 `;
 
 const StImage = styled.img`
@@ -48,8 +44,6 @@ const StPostUserId = styled(Link)`
 const StLikeButton = styled.button``;
 
 const StPopularity = styled.span`
-  /* background-color: aqua; */
-  /* background-color: red; */
   display: flex;
   align-items: center;
   gap: 5px;
@@ -63,7 +57,6 @@ const StPopularity = styled.span`
 `;
 
 const StPostContentWrapper = styled.div`
-  /* background-color: green; */
   font-size: 80%;
   width: 100%;
   height: 200%;
@@ -71,14 +64,12 @@ const StPostContentWrapper = styled.div`
 `;
 
 const StPostContent = styled.p`
-  /* background-color: red; */
   height: 25px;
   overflow: hidden;
   white-space: normal;
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 2;
-  /* text-align: justify; */
   color: #7e7e7e;
 
   @media screen and (max-width: 600px) {
@@ -87,48 +78,121 @@ const StPostContent = styled.p`
 `;
 
 const StPostDate = styled.div`
-  /* background-color: aliceblue; */
   width: 100%;
   display: flex;
   justify-content: left;
   color: #d0d0d0;
-  /* position: absolute;
-  bottom: 1%; */
   @media screen and (max-width: 600px) {
     font-size: 50%;
   }
 `;
+
 const PostItem = ({ post }) => {
   const { id, product_imageSrc, product_name, post_content, popularity, created_at } = post;
-
+  const [popularityNum, setPopularityNum] = useState(popularity);
   const createdAt = created_at;
+  const [heartChk, setHeartChk] = useState(false);
+  const [saveChk, setSaveChk] = useState(false);
 
   const createDate = `${createdAt.slice(0, 10)} ${createdAt.slice(11, 19)}`;
 
-  const hendleHeartUp = async () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      let loginChk = await checkSignIn();
+      if (!loginChk) return false;
+      let user = await getUser();
+
+      const { data: userInfo } = await supabase.from('user_info').select('*').eq('user_id', user.email);
+      if (userInfo && userInfo.length > 0) {
+        const heart = JSON.parse(userInfo[0].post_heart).find((heartId) => heartId === id);
+        const save = JSON.parse(userInfo[0].post_save).find((saveId) => saveId === id);
+
+        heart && setHeartChk(true);
+        save && setSaveChk(true);
+      }
+    };
+    fetchUser();
+  }, [id]);
+
+  const handleHeartUp = async () => {
     let loginChk = await checkSignIn();
+    if (!loginChk) return alert('로그인 후에 이용 가능합니다.');
     let user = await getUser();
-    console.log(loginChk, user);
-    if (!loginChk) return alert('로그인 후에 이용 가능합니다.');
 
-    const { data: userInfo, error } = await supabase.from('user_info').select('*').eq('user_id', user.id);
+    const { data: userInfo } = await supabase.from('user_info').select('*').eq('user_id', user.email);
+    if (userInfo && userInfo.length > 0) {
+      let heart = JSON.parse(userInfo[0].post_heart);
+      if (heartChk) {
+        console.log('-');
 
-    console.log(userInfo);
+        const { data, error } = await supabase
+          .from('posts')
+          .update({
+            popularity: popularityNum - 1
+          })
+          .eq('id', id);
+        if (error) {
+          console.error('Error updating popularity:', error);
+          return;
+        }
+        heart = heart.filter((heartId) => heartId !== id);
+        console.log(heart);
 
-    // const { data, error } = await supabase
-    // .from('posts')
-    // .update({
-    //   popularity: popularity+1,
-    // })
-    // .eq('id', id);
+        setPopularityNum((num) => num - 1);
+        setHeartChk(false);
+      } else {
+        heart.push(id);
+        console.log(heart);
+
+        const { data, error } = await supabase
+          .from('posts')
+          .update({
+            popularity: popularityNum + 1
+          })
+          .eq('id', id);
+        if (error) {
+          console.error('Error updating popularity:', error);
+          return;
+        }
+        setPopularityNum((num) => num + 1);
+        setHeartChk(true);
+      }
+
+      const { data: hearts, error: heartsError } = await supabase
+        .from('user_info')
+        .update({ post_heart: JSON.stringify(heart) })
+        .eq('user_id', user.email);
+      if (heartsError) {
+        console.error('Error updating user hearts:', heartsError);
+      }
+    }
   };
 
-  const hendleSaveUp = async () => {
+  const handleSaveUp = async () => {
     let loginChk = await checkSignIn();
     if (!loginChk) return alert('로그인 후에 이용 가능합니다.');
-  };
+    let user = await getUser();
 
-  // const [activeButton, setActivButton] = useState(null);
+    const { data: userInfo } = await supabase.from('user_info').select('*').eq('user_id', user.email);
+    if (userInfo && userInfo.length > 0) {
+      let save = JSON.parse(userInfo[0].post_save);
+      if (saveChk) {
+        save = save.filter((saveId) => saveId !== id);
+        setSaveChk(false);
+      } else {
+        save.push(id);
+        setSaveChk(true);
+      }
+
+      const { data: saves, error: savesError } = await supabase
+        .from('user_info')
+        .update({ post_save: JSON.stringify(save) })
+        .eq('user_id', user.email);
+      if (savesError) {
+        console.error('Error updating user saves:', savesError);
+      }
+    }
+  };
 
   return (
     <StPostItem>
@@ -136,13 +200,11 @@ const PostItem = ({ post }) => {
         <StImage src={product_imageSrc} alt={product_name} />
       </Link>
       <StIdAndLikeButtons>
-        <StPostUserId to="/detailed">닉네임</StPostUserId>
+        <StPostUserId to="/detailed">{product_name}</StPostUserId>
         <StPopularity>
-          <StLikeButton onClick={hendleHeartUp}>
-            <IoHeartOutline />
-          </StLikeButton>
-          {popularity}
-          <IoBookmarkOutline color="red" size={27} onClick={() => {}} />
+          <StLikeButton onClick={handleHeartUp}>{heartChk ? <p>ㅁ</p> : <IoHeartOutline />}</StLikeButton>
+          {popularityNum}
+          <IoBookmarkOutline color={saveChk ? 'red' : 'yellow'} size={27} onClick={handleSaveUp} />
         </StPopularity>
       </StIdAndLikeButtons>
       <StPostContentWrapper>
@@ -152,4 +214,5 @@ const PostItem = ({ post }) => {
     </StPostItem>
   );
 };
+
 export default PostItem;
